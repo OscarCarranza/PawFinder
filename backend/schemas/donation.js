085710473b120cf51c574685e0601434905ad688
OscarCarranza <oscar.carranza@unitec.edu>
2016-06-16 13:31:14 -0600

    Fixing

--- backend/schemas/donation.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var DonationSchema = new mongoose.Schema({
-  donatorName: String,
-  amount: Number,
-  date: String,
-});
-
-module.exports = mongoose.model('Donation', DonationSchema);
